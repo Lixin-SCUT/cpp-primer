a1efe6d250be88c8f1071ed10de36b04f0f132b3
Lixin-ee <im.lixin@foxmail.com>
2019-12-05 15:56:13 +0800

    Add files via upload

---
+++ chapter17/homework17.34.cpp
@@ -0,0 +1,41 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include<vector>
+#include<string>
+#include<list>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+#include<iterator>
+#include<fstream>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<sstream>
+#include<memory>
+#include<utility>
+#include<iomanip>
+using namespace std;
+
+int main() {
+	cout << noboolalpha << true << " " << boolalpha << true << endl;
+	cout << hex << 20 <<" "<< showbase << 20 << endl<<dec;
+	cout << 1.000000000000000 <<" "<< showpoint << 1.00000000000000 << endl;
+	cout << 1 << " " << showpos <<1 << endl;
+
+	cout << setfill('#') << "i:" << setw(12) << 16  << endl ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ chapter17/homework17.36.cpp
@@ -0,0 +1,36 @@
+#pragma warning(disable:4996)
+#include <iostream>
+#include<vector>
+#include<string>
+#include<list>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+#include<iterator>
+#include<fstream>
+#include<map>
+#include<set>
+#include<unordered_map>
+#include<sstream>
+#include<memory>
+#include<utility>
+#include<iomanip>
+using namespace std;
+
+int main() {
+	cout << hexfloat <<uppercase<<internal<< sqrt(2.0) << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
